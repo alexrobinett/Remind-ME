ff812a5a22d6a6b0993f09615efb222884b9566b
robinetta <alxr19@gmail.com>
2023-02-03 22:09:45 -0500

    setup fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {addListeners} from './JS/buttons.js'
 import { createReminder, Project, projectsContainer, getProjectsFromStore } from './JS/control'
 import { displayProjectList, displayProjectTitle, displayReminders, renderRemindersList } from './JS/display'
 
-import '.style.css'
+import './style.css'
 import EditBtn from './icons/text-box-edit.svg'
 import deleteBtn from './icons/trash-can-outline.svg'
 import inboxIcon from './icons/inbox.svg'
